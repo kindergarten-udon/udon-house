3283d79214ba4b4162a44dca3253b1ad45755d02
yoonho <dydgh179@gmail.com>
2023-03-14 21:38:29 +0900

    [FEAT] 상세 게시글 페이지 좋아요 버튼 toggle 완성

--- src/components/Community/BoardItem.js
+++ src/components/Community/BoardItem.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { BsHandThumbsUp } from "react-icons/bs";
+import { BsHandThumbsUpFill } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
 import boardData from "./boardData";
 
@@ -18,7 +19,12 @@ const getTitleByIndex = (index) => {
 const getContentByIndex = (index) => {
   return boardData[index].내용;
 };
+
 const BoardItemDetail = ({ title, content }) => {
+  const [isChecked, setChecked] = useState(false);
+  const toggleMenu = () => {
+    setChecked((isChecked) => !isChecked);
+  };
   return (
     <>
       <section className=" py-[3%] lg:py-[5%] mx-auto w-[70vw] min-w-[280px] lg:min-w-[500px] text-gray-700">
@@ -26,13 +32,12 @@ const BoardItemDetail = ({ title, content }) => {
         <div className="h-[300px] px-6 py-2 text-left border border-gray-500 rounded-3xl">{content}</div>
         <div className="text-sm lg:text-base my-4 mx-4 flex justify-between">
           <div className="">
+            {/* {만약 이 글을 쓴 사람이라면 수정하기, 삭제하기가 보이게하기} */}
             <button className="mr-4">수정하기</button>
             <button>삭제하기</button>
           </div>
           <div className="flex">
-            <button>
-              <BsHandThumbsUp></BsHandThumbsUp>
-            </button>
+            <button onClick={toggleMenu}>{isChecked ? <BsHandThumbsUpFill /> : <BsHandThumbsUp />}</button>
             <Link className="ml-4 flex " to="/Community">
               <GiHamburgerMenu className="block h-5"></GiHamburgerMenu> 목록
             </Link>
