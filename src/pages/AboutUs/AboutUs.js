069286c0702b1949be1d69acac9063cd2c4fa7c0
Sumin Kim <ksm6399@gmail.com>
2023-03-15 05:01:33 +0900

    [FEAT] About Us 페이지 반응형 구현 #9

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -10,29 +10,27 @@ const AboutUs = () => {
 
   return (
     <>
-      <div className="wrapper lg:py-[120px] py-[72px] w-full">
+      <div className="wrapper lg:py-[150px] py-[100px] w-full">
         <span className="text-[40px] px-8 py-2 font-bold pt-16 inline-block border-b-4 border-black">About Us</span>
-        <div className="py-[80px] flex gap-12 justify-center ">
-          <div>
+        <div className="p-[80px] lg:flex lg:gap-12 lg:justify-center md:grid md:grid-cols-3 md:gap-28 grid grid-cols-1 gap-32 place-items-center">
+          <div className="">
             <figure className="w-[180px] h-[180px] relative">
               <img src="/iNaraChar.png" alt="" className="absolute w-10 h-20 left-[-5px] top-[-18px] rotate-45 -z-50" />
               <div className="flex flex-col">
                 <img src="/JuHyeong.jpg" alt="주형사진" className="rounded-full w-full h-full object-cover -z-10" />
-                <figcaption className="mt-5 text-lg -z-10">
+                <figcaption className="mt-5 text-lg block">
                   이주형
-                  <a href="mailto:" className="block">
-                    Email
-                  </a>
+                  <a href="mailto:test@google.co.kr">test@google.co.kr</a>
                 </figcaption>
               </div>
             </figure>
           </div>
-          <div>
+          <div className="">
             <figure className="w-[180px] h-[180px] relative">
               <img src="/noticeImage.png" alt="" className="absolute w-30 h-20 left-[-25px] top-[-50px] -rotate-12 -z-50" />
               <div className="flex flex-col">
                 <img src="/JuHyeong.jpg" alt="주형사진" className="rounded-full w-full h-full object-cover -z-10" />
-                <figcaption className="mt-5 text-lg -z-10">
+                <figcaption className="mt-5 text-lg ">
                   유하리
                   <a href="mailto:" className="block">
                     Email
@@ -41,12 +39,12 @@ const AboutUs = () => {
               </div>
             </figure>
           </div>
-          <div>
+          <div className="">
             <figure className="w-[180px] h-[180px] relative">
               <img src="/iNaraChar.png" alt="" className="absolute w-10 h-20 left-[-5px] top-[-18px] rotate-45 -z-50" />
               <div className="flex flex-col w-[180px] h-[180px]">
                 <img src="/JaeWoo.jpeg" alt="재우사진" className="rounded-full w-full h-full object-cover object-top -z-10" />
-                <figcaption className="mt-5 text-lg -z-10">
+                <figcaption className="mt-5 text-lg">
                   장재우
                   <a href="mailto:" className="block">
                     Email
@@ -60,7 +58,7 @@ const AboutUs = () => {
               <img src="/iNaraChar.png" alt="" className="absolute w-10 h-20 left-[-5px] top-[-18px] rotate-45 -z-50" />
               <div className="flex flex-col w-[180px] h-[180px]">
                 <img src="/YoonHo.jpg" alt="윤호사진" className="rounded-full w-full h-full object-cover -z-10" />
-                <figcaption className="mt-5 text-lg -z-10">
+                <figcaption className="mt-5 text-lg">
                   조윤호
                   <a href="mailto:" className="block">
                     Email
@@ -74,7 +72,7 @@ const AboutUs = () => {
               <img src="/noticeImage.png" alt="" className="absolute w-30 h-20 left-[-25px] top-[-50px] -rotate-12 -z-50" />
               <div className="flex flex-col w-[180px] h-[180px]">
                 <img src="/SuMin.png" alt="수민사진" className="rounded-full w-full h-full object-cover -z-10" />
-                <figcaption className="mt-5 text-lg -z-10">
+                <figcaption className="mt-5 text-lg">
                   김수민
                   <a href="mailto:" className="block">
                     Email
