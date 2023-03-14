36f010edef7ec3f090fbf2c273fe6a13d66ec251
Sumin Kim <ksm6399@gmail.com>
2023-03-15 05:00:41 +0900

    [Feat] Footer Link 연결 #8

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,9 @@ import { Nav } from "components/Nav/Nav";
 const Footer = () => {
   return (
     <>
-      <footer className="w-full lg:h-[clamp(350px,30vw,476px)] h-[535px] lg:text-[clamp(12px,1.3vw,16px)] text-base bg-bg-gray-color">
+      <footer className="w-full lg:h-[clamp(350px,30vw,476px)] h-[535px] lg:text-[clamp(12px,1.3vw,16px)] text-base bg-bg-gray-color box-border">
         {/* <div className="lg:h-[7%] h-[32px] bg-white rounded-b-[30px]"></div> */}
-        <div className="w-full h-[93%] lg:p-[clamp(32px,4%,60px)] px-[32px] py-[48px]">
+        <div className="w-full h-full lg:p-[clamp(32px,4.5%,80px)] px-[32px] py-[48px] box-border">
           <div className="flex lg:flex-row lg:justify-between flex-col">
             <Nav className="text-left leading-[30px] font-medium" />
             <div className="flex gap-3">
@@ -16,13 +16,17 @@ const Footer = () => {
               </Link>
             </div>
           </div>
-          <div className="footer-info__wrapper lg:mt-[clamp(45px,4vw,64px)] mt-[55px]">
+          <div className="footer-info__wrapper lg:mt-[clamp(15px,5%,80px)] mt-[55px]">
             <div className="footer-info text-left">
               <div className="flex gap-5">
-                <span>이용약관</span>
-                <b>개인정보처리방침</b>
+                <Link to="https://www.i-nara.co.kr/terms" target="_blank">
+                  <span>이용약관</span>
+                </Link>
+                <Link to="https://www.i-nara.co.kr/terms/privacy" target="_blank">
+                  <b>개인정보처리방침</b>
+                </Link>
               </div>
-              <div className="lg:flex lg:gap-3 lg:mt-[clamp(35px,3.5vw,64px)] mt-[20px]">
+              <div className="lg:flex lg:gap-3 lg:mt-[clamp(35px,4%,64px)] mt-[20px]">
                 <p>(주)우동집</p>
                 <span className="lg:inline-block hidden">|</span>
                 <p>대표이사 우동</p>
