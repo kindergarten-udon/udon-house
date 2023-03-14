deb36bc95028f7503615ed2187c52313df8704d6
oweaj <qwe1520156@naver.com>
2023-03-14 21:08:38 +0900

    [FEAT] select태그 삭제 -> react-select 적용

--- src/components/MapInfo/KindergartenList.js
+++ src/components/MapInfo/KindergartenList.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Select from "react-select";
 import { BiSearch, BiMap, BiInfoCircle } from "react-icons/bi";
 import KindergartenModal from "components/MapInfo/KindergartenModal";
 
@@ -40,19 +41,15 @@ const KindergartenList = ({ modalShow }) => {
     <div className="flex-0.5 lg:flex-1">
       <div className="py-7 bg-main-color">
         <img src="/kindergarten.svg" className="mx-auto" />
-        <div className="whitespace-nowrap text-sm">
-          <select name="city" defaultValue="selected" className="w-[4.5rem] h-8 rounded-md px-1 lg:w-24 lg:text-base">
-            <option value="selected">자치구</option>
-            <option value="1">전체</option>
-            {/* option 요소들 api 자치구정보 받고 map으로 뿌려주기  */}
-          </select>
-          <select name="type" defaultValue="selected" className="w-[6.5rem] h-8 rounded-md px-1 mx-2 lg:w-32 lg:text-base lg:mx-7">
-            <option value="selected">어린이집 유형</option>
-            <option value="1">전체</option>
-            {/* option 요소들 api 어린이집 유형별로 받고 map으로 뿌려주기  */}
-          </select>
-          <input type="text" placeholder="어린이집 이름을 입력해주세요." className="w-40 h-8 placeholder:text-xs rounded-md px-2 mr-1 lg:w-48 lg:placeholder:text-sm"></input>
-          <BiSearch className="inline-block cursor-pointer" />
+        <div className="flex flex-row items-center justify-center whitespace-nowrap text-sm px-2 gap-2 lg:gap-4">
+          <Select className="w-24 lg:w-28 lg:text-base" placeholder="자치구" />
+          {/* option 요소들 api 자치구정보 받고 map으로 뿌려주기  */}
+
+          <Select className="w-32 lg:w-44 lg:text-base" placeholder="어린이집유형" />
+          {/* option 요소들 api 어린이집 유형별로 받고 map으로 뿌려주기  */}
+
+          <input type="text" placeholder="어린이집을 입력해주세요." className="w-44 h-9 rounded-md px-2 lg:w-48"></input>
+          <BiSearch className="lg:w-5 lg:h-5 inline-block cursor-pointer" />
         </div>
       </div>
       <div className="text-left">
