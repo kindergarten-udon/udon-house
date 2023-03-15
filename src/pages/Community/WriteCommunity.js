f03a9dacceaf4041dc46725b61db75b4eb2e57fd
yoonho <dydgh179@gmail.com>
2023-03-15 16:41:59 +0900

    [FEAT] 게시글 작성시 공백이면 alert창 생성 #30

--- src/pages/Community/WriteCommunity.js
+++ src/pages/Community/WriteCommunity.js
@@ -1,5 +1,5 @@
 import "./Community.css";
-import { React, useState } from "react";
+import { React, useState, createRef } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import CancelButton from "components/Community/CancelButton";
@@ -7,6 +7,9 @@ import WriteButton from "components/Community/WriteButton";
 const WriteCommunity = () => {
   const [cancelModalOpen, setcancelModalOpen] = useState(false);
   const [WriteModalOpen, setWriteModalOpen] = useState(false);
+
+  const titleRef = createRef();
+  const contentRef = createRef();
 
   const showCancelModal = () => {
     if (setcancelModalOpen === true) {
@@ -16,7 +19,13 @@ const WriteCommunity = () => {
     }
   };
   const showWriteModal = () => {
-    setWriteModalOpen(true);
+    const inputValue = titleRef.current.value;
+    const contentValue = contentRef.current.value;
+    if (inputValue.trim() == "" || contentValue.trim() == "") {
+      alert("제목과 내용을 입력하세요.");
+    } else {
+      setWriteModalOpen(true);
+    }
   };
 
   return (
@@ -31,9 +40,9 @@ const WriteCommunity = () => {
       <h2 className="font-bold  text-gray-700 text-xl lg:text-2xl mt-8 mb-8">우리 아이들의 궁금한점을 질문해보세요</h2>
       <div className="title-wrapper items-center  flex justify-center">
         <h3 className="w-[10vw] text-center font-bold text-xl lg:text-2xl">제목</h3>
-        <input form="text" placeholder="제목을 적어주세요." className="text-sm lg:text-base w-[50vw] lg:w-[45vw] p-4 border rounded-2xl border-slate-700" />
+        <input ref={titleRef} form="text" placeholder="제목을 적어주세요." className="text-sm lg:text-base w-[50vw] lg:w-[45vw] p-4 border rounded-2xl border-slate-700" />
       </div>
-      <textarea placeholder="내용을 적어주세요." className="text-sm lg:text-base mt-4 w-[60vw] lg:w-[55vw] h-[40vh] p-4 border rounded-2xl border-slate-700" />
+      <textarea ref={contentRef} placeholder="내용을 적어주세요." className="text-sm lg:text-base mt-4 w-[60vw] lg:w-[55vw] h-[40vh] p-4 border rounded-2xl border-slate-700" />
       <div className="mt-6 lg:mt-8 text-sm lg:text-base">
         <button className="mr-3 lg:mr-5 bg-white px-9 lg:px-12 py-2 border border-slate-700 rounded-3xl" onClick={showCancelModal}>
           취소
