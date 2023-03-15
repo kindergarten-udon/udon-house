24f694d850af1c56475f8428a18e69cc6c4f648e
yoonho <dydgh179@gmail.com>
2023-03-15 09:44:26 +0900

    [STYLE] 폰트변경

--- src/components/Community/BoardListItem.js
+++ src/components/Community/BoardListItem.js
@@ -7,7 +7,7 @@ import { IoIosArrowDown } from "react-icons/io";
 function BoardItem({ index, title, content }) {
   return (
     <div className="text-left  flex py-2 lg:py-3 border border-transparent border-b-gray-300 w-full">
-      <span className="text-gray-500 text-center w-[15vw] font-bold">{title}</span>
+      <span className="text-gray-500 text-center w-[15vw] font-medium">{title}</span>
       <Link className="w-full px-3 lg:px-0 overflow-hidden" to={`${index}`}>
         {content}
       </Link>
--- src/pages/Community/Community.js
+++ src/pages/Community/Community.js
@@ -9,7 +9,7 @@ const Community = () => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <div className=" lg:pt-[120px] pt-[72px] container w-screen min-w-full ">
+    <div className=" lg:pt-[120px] pt-[72px] container w-screen min-w-full text-gray-700 ">
       <h2 className="text-2xl lg:text-5xl font-bold text-left lg:px-[10vw] px-7 mt-7 text-gray-800 bg-">커뮤니티</h2>
       <div className="text-sm lg:text-base text-left lg:px-[10vw] px-7 mt-3 lg:mt-6 mb-2 lg:mb-5 text-gray-800">우리아이 자랑 커뮤니티</div>
       <section className="bg-light-yellow-color lg:px-[10vw] px-7 py-5">
