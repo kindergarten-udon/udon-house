2a1424bccf5e567e433e044ecad0922ffe41c070
oweaj <qwe1520156@naver.com>
2023-03-15 14:48:23 +0900

    [FIX] map 확대/축소 할때 처음 설정한 위치가 보이는 현상 수정

--- src/components/MapInfo/KindergartenMap.js
+++ src/components/MapInfo/KindergartenMap.js
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 const { kakao } = window;
 
 const KindergartenMap = () => {
   useEffect(() => {
     let container = document.getElementById("map");
     let options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      center: new kakao.maps.LatLng(37.56647, 126.977963),
       level: 3,
     };
     let map = new kakao.maps.Map(container, options);
 
-    let markerPoint = new kakao.maps.LatLng(33.450701, 126.570667);
+    let markerPoint = new kakao.maps.LatLng(37.56647, 126.977963);
     let marker = new kakao.maps.Marker({
       position: markerPoint,
     });
