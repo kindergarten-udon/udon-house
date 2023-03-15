c7f7890feff60fc3c5cf806daf8ce72add8c0fa1
oweaj <qwe1520156@naver.com>
2023-03-15 16:43:27 +0900

    [FEAT] 지도 다중 마커 클릭 이벤트 진행중

--- src/components/MapInfo/KindergartenMap.js
+++ src/components/MapInfo/KindergartenMap.js
@@ -1,20 +1,56 @@
 import React, { useEffect, useRef } from "react";
 const { kakao } = window;
 
+const testPosition = [
+  {
+    title: "카카오",
+    latlng: new kakao.maps.LatLng(33.450705, 126.570677),
+  },
+  {
+    title: "생태연못",
+    latlng: new kakao.maps.LatLng(33.450936, 126.569477),
+  },
+  {
+    title: "텃밭",
+    latlng: new kakao.maps.LatLng(33.450879, 126.56994),
+  },
+  {
+    title: "근린공원",
+    latlng: new kakao.maps.LatLng(33.451393, 126.570738),
+  },
+];
+
 const KindergartenMap = () => {
   useEffect(() => {
     let container = document.getElementById("map");
     let options = {
-      center: new kakao.maps.LatLng(37.56647, 126.977963),
+      center: new kakao.maps.LatLng(33.450705, 126.570677),
       level: 3,
     };
     let map = new kakao.maps.Map(container, options);
 
-    let markerPoint = new kakao.maps.LatLng(37.56647, 126.977963);
-    let marker = new kakao.maps.Marker({
-      position: markerPoint,
+    testPosition.map(({ title, latlng }) => {
+      let imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+      let imageSize = new kakao.maps.Size(24, 35);
+      let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+      let info = `<div class="text-center">${title}</div>`;
+      let infoPosition = new kakao.maps.LatLng(33.450705, 126.570677);
+
+      let infowindow = new kakao.maps.InfoWindow({
+        position: infoPosition,
+        content: info,
+      });
+
+      let marker = new kakao.maps.Marker({
+        map: map,
+        position: latlng,
+        // title: title,
+        image: markerImage,
+      });
+
+      infowindow.open(map, marker);
     });
-    marker.setMap(map);
 
     let zoomControl = new kakao.maps.ZoomControl();
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
