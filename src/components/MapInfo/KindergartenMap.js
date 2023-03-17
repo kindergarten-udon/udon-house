3daa127cdd86f5efb9120767b9e154929d020c8f
oweaj <qwe1520156@naver.com>
2023-03-18 01:33:30 +0900

    [FIX] 지도 레벨 수정 및 지도 중심 좌표 다시 지정

--- src/components/MapInfo/KindergartenMap.js
+++ src/components/MapInfo/KindergartenMap.js
@@ -5,8 +5,8 @@ const KindergartenMap = ({ kinderList }) => {
   useEffect(() => {
     let container = document.getElementById("map");
     let options = {
-      center: new kakao.maps.LatLng(37.555949, 126.972329),
-      level: 8,
+      center: new kakao.maps.LatLng(37.566824, 126.978652),
+      level: 5,
     };
     let map = new kakao.maps.Map(container, options);
 
@@ -15,7 +15,7 @@ const KindergartenMap = ({ kinderList }) => {
       let imageSize = new kakao.maps.Size(24, 35);
       let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
-      let infoContent = `<div style="margin:5px 35px; white-space: nowrap; color:orange">${CRNAME}</div>`;
+      let infoContent = `<div style="margin:5px 35px; white-space:nowrap; color:orange">${CRNAME}</div>`;
       let infoPosition = new kakao.maps.LatLng(LA, LO);
       let iwRemoveable = true;
 
