291a5ccf30fcbb090426173a2bd5de385b380f7d
yoonho <dydgh179@gmail.com>
2023-03-21 23:40:30 +0900

    [FEAT] MyPage 추가, 마크업 진행, Header 변경

--- src/components/MyPage/MyBoard.js
+++ src/components/MyPage/MyBoard.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { userData } from "atom/atom";
+import { useRecoilState } from "recoil";
 
-const MyBoard = ({ setActive }) => {
-  return <div>내가 쓴 글</div>;
+const MyBoard = ({ setActive, userId }) => {
+  const content = useRecoilState(userData);
+  const myBoard = content[0].filter((el) => {
+    return el.creatorId === userId.uid;
+  });
+  console.log(myBoard);
+
+  return (
+    <div>
+      {myBoard.map((el) => {
+        return (
+          <div key={el.id}>
+            <span>{el.title}</span>
+            <span>{el.content}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MyBoard;
--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,14 +1,29 @@
 import Favorite from "components/MyPage/Favorite";
 import MyBoard from "components/MyPage/MyBoard";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const MyPage = () => {
+const MyPage = ({ userId }) => {
   const [active, setActive] = useState(false);
+
   return (
-    <div className="p-[200px]">
-      <div>프로필 영역</div>
+    <div className="lg:pt-[120px] pt-[72px]">
+      <section className="relative mb-10 text-center">
+        <div className="bg-main-color h-[50px] lg:h-[70px] z-10"></div>
+        <div className="bg-subMain-color h-[150px] lg:h-[250px]">
+          <img src="/characters.png" alt="배경" className="w-screen max-sm:h-0 h-[150px] lg:h-[250px]" />
+          <img src="/gom.png" alt="곰" className="max-sm:w-40 max-sm:-translate-y-4 w-0 -z-1" />
+        </div>
+        <div className="absolute top-2 max-sm:top-1 left-1/2 -translate-x-[50%] mx-auto w-32 lg:w-52 text-sm lg:text-base">
+          {/* userId의 사진이 들어와야함.  */}
+          <img src="/JuHyeong.jpg" alt="주형사진" className="aboutImgae px-2 py-1 border-gray-700" />
+          <button className="border border-gray-700 rounded-2xl bg-white px-2 py-1 my-3 ">프로필 사진 편집</button>
+          <br />
+          {userId && userId.email}
+        </div>
+      </section>
+      <button></button>
       <Favorite setActive={setActive} />
-      <MyBoard setActive={setActive} />
+      <MyBoard setActive={setActive} userId={userId} />
     </div>
   );
 };
