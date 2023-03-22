992695eb3cc864d9768dbb7149284d2673467612
yoonho <dydgh179@gmail.com>
2023-03-22 15:38:20 +0900

    [FEAT] 마이페이지 내가작성한글 컴포넌트 교체 완성

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -3,8 +3,7 @@ import MyBoard from "components/MyPage/MyBoard";
 import React, { useState, useEffect } from "react";
 
 const MyPage = ({ userId }) => {
-  // const [active, setActive] = useState("1");
-  const [active, setActive] = useState(true);
+  const [active, setActive] = useState(false);
 
   const handleOptionChange = (event) => {
     setActive(event.target.id);
@@ -39,37 +38,15 @@ const MyPage = ({ userId }) => {
           {userId && userId.email}
         </div>
       </section>
-      {/* <div className="translate-x-3">
-        <input type="radio" className="peer hidden" name="option" id="1" checked={active === "1"} onChange={handleOptionChange} />
-        {active === "1" ? (
-          <label htmlFor="1" className="text-2xl lg:text-3xl max-sm:mr-[10%] max-sm:text-xl hover:cursor-pointer text-gray-600 border-b-4 border-green-200 mr-20">
-            내가 작성한 글
-          </label>
-        ) : (
-          <label htmlFor="1" className="text-2xl lg:text-3xl max-sm:mr-[10%] max-sm:text-xl hover:cursor-pointer hover:text-gray-600 hover:border-b-4 hover:border-green-200 mr-20">
-            내가 작성한 글
-          </label>
-        )}
-        <input type="radio" className="peer hidden ml-10" name="option" id="2" checked={active === "2"} onChange={handleOptionChange} />
-        {active === "2" ? (
-          <label htmlFor="2" className="text-2xl lg:text-3xl max-sm:mr-[10%] max-sm:text-xl hover:cursor-pointer hover:text-gray-600 border-b-4 border-green-200 ">
-            어린이집 즐겨찾기
-          </label>
-        ) : (
-          <label htmlFor="2" className="text-2xl lg:text-3xl max-sm:mr-[10%] max-sm:text-xl hover:cursor-pointer hover:text-gray-600 hover:border-b-4 hover:border-green-200 ">
-            어린이집 즐겨찾기
-          </label>
-        )}
-      </div> */}
-
-      <>
-        <button name="myboard" onClick={testClick} className={!active ? "bg-red-600" : ""}>
+      <div className="max-sm:text-xl text-2xl lg:text-3xl max-sm:translate-x-1 translate-x-5">
+        <button name="myboard" onClick={testClick} className={!active ? "mypageChecked max-sm:mr-[10%] mr-20 " : "mypage max-sm:mr-[10%] mr-20"}>
           내가 작성한글
         </button>
-        <button name="favorite" onClick={testClick} className={active ? "bg-black" : ""}>
-          즐겨찾기
+        <button name="favorite" onClick={testClick} className={active ? "mypageChecked " : "mypage "}>
+          어린이집 즐겨찾기
         </button>
-      </>
+      </div>
+
       {active ? <Favorite setActive={setActive} /> : <MyBoard setActive={setActive} userId={userId} />}
     </div>
   );
