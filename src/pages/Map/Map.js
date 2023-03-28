0576a76124090fea4c1d49aa8551062d75093c94
leejh4197 <leejh4197@naver.com>
2023-03-28 16:51:13 +0900

    [FIX]mixed content이슈 테스트

--- src/pages/Map/Map.js
+++ src/pages/Map/Map.js
@@ -13,26 +13,26 @@ const Map = ({ userId }) => {
   const [map, setMap] = useState(null);
   const [favoriteData, setFavoriteData] = useState(null);
 
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     try {
-  //       const url = `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_KINDERINFO_KEY}/json/ChildCareInfo/1/1000/`;
-  //       const response = await axios.get(url);
-  //       let arr = response.data.ChildCareInfo.row;
-  //       let filtered = arr.filter((elem) => elem.CRSTATUSNAME !== "폐지" && elem.STCODE !== "11545000341" && elem.STCODE !== "11380000668");
-  //       setKinderList(filtered);
-  //     } catch (error) {
-  //       alert("데이터를 불러오는 과정에서 에러가 발생했습니다!!");
-  //     }
-  //   };
-  //   getData();
-  // }, []);
-
   useEffect(() => {
-    let arr = data[0].ChildCareInfo.row;
-    let filtered = arr.filter((elem) => elem.CRSTATUSNAME !== "폐지" && elem.STCODE !== "11545000341" && elem.STCODE !== "11380000668");
-    setKinderList(filtered);
+    const getData = async () => {
+      try {
+        const url = `api/${process.env.REACT_APP_KINDERINFO_KEY}/json/ChildCareInfo/1/1000/`;
+        const response = await axios.get(url);
+        let arr = response.data.ChildCareInfo.row;
+        let filtered = arr.filter((elem) => elem.CRSTATUSNAME !== "폐지" && elem.STCODE !== "11545000341" && elem.STCODE !== "11380000668");
+        setKinderList(filtered);
+      } catch (error) {
+        alert("데이터를 불러오는 과정에서 에러가 발생했습니다!!");
+      }
+    };
+    getData();
   }, []);
+
+  // useEffect(() => {
+  //   let arr = data[0].ChildCareInfo.row;
+  //   let filtered = arr.filter((elem) => elem.CRSTATUSNAME !== "폐지" && elem.STCODE !== "11545000341" && elem.STCODE !== "11380000668");
+  //   setKinderList(filtered);
+  // }, []);
 
   const modalShow = (e) => {
     if (!(e.target.tagName === "svg" || e.target.tagName === "path")) {
