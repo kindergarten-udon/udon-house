2f9f528d8876080a32e627d4534ec4fae36a7b35
leejh4197 <leejh4197@naver.com>
2023-03-28 13:01:38 +0900

    [FIX]meta추가

--- src/pages/Map/Map.js
+++ src/pages/Map/Map.js
@@ -15,8 +15,7 @@ const Map = ({ userId }) => {
   useEffect(() => {
     const getData = async () => {
       try {
-        // const url = `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_KINDERINFO_KEY}/json/ChildCareInfo/1/1000/`;
-        const url = `/api`;
+        const url = `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_KINDERINFO_KEY}/json/ChildCareInfo/1/1000/`;
         const response = await axios.get(url);
         let arr = response.data.ChildCareInfo.row;
         let filtered = arr.filter((elem) => elem.CRSTATUSNAME !== "폐지" && elem.STCODE !== "11545000341" && elem.STCODE !== "11380000668");
