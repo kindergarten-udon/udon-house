0bf68eae688b95e2adb1f91c48ed154d88dfd5a5
oweaj <qwe1520156@naver.com>
2024-01-08 23:42:09 +0900

    [CHORE] 성능 Refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
+import React, { lazy, Suspense } from "react";
 import "./App.css";
-import Map from "pages/Map/Map";
 import { auth, dbService } from "util/fbase";
-import Main from "pages/Main/Main";
 import SignUp from "pages/SignUp/SignUp";
 import SignIn from "pages/SignIn/SignIn";
 import MyPage from "pages/MyPage/MyPage";
-import AboutUs from "pages/AboutUs/AboutUs";
 import Header from "components/Header/Header";
 import Footer from "components/Footer/Footer";
 import NotFound from "pages/NotFound/NotFound";
-import Community from "pages/Community/Community";
 import { useEffect, useState } from "react";
 import WriteCommunity from "pages/Community/WriteCommunity";
 import { Routes, Route, Navigate } from "react-router-dom";
@@ -18,6 +15,10 @@ import { collection } from "firebase/firestore";
 import { useSetRecoilState } from "recoil";
 import { uid, userData } from "Atom/atom";
 import { RecoilLogger } from "recoil-devtools-logger";
+const Main = lazy(() => import("pages/Main/Main"));
+const Map = lazy(() => import("pages/Map/Map"));
+const AboutUs = lazy(() => import("pages/AboutUs/AboutUs"));
+const Community = lazy(() => import("pages/Community/Community"));
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
@@ -57,18 +58,20 @@ function App() {
     <div className="App font-sans">
       <RecoilLogger values={[uid, userData]} />
       <Header isLogin={isLogin} userId={userId} userProfile={userProfile} setUserProfile={setUserProfile} />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/map" element={<Map userId={userId} />} />
-        <Route path="/signup" element={<PrivateRoute component={<SignUp />} userId={userId} />} />
-        <Route path="/signin" element={<PrivateRoute component={<SignIn />} userId={userId} />} />
-        <Route path="/mypage" element={<MyPage userId={userId} userProfile={userProfile} setUserProfile={setUserProfile} />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/community" element={<Community isLogin={isLogin} />} />
-        <Route path="/community/:id" element={<Community userId={userId} />} />
-        <Route path="/writeCommunity" element={<WriteCommunity userId={userId} />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loding...</div>}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/map" element={<Map userId={userId} />} />
+          <Route path="/signup" element={<PrivateRoute component={<SignUp />} userId={userId} />} />
+          <Route path="/signin" element={<PrivateRoute component={<SignIn />} userId={userId} />} />
+          <Route path="/mypage" element={<MyPage userId={userId} userProfile={userProfile} setUserProfile={setUserProfile} />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/community" element={<Community isLogin={isLogin} />} />
+          <Route path="/community/:id" element={<Community userId={userId} />} />
+          <Route path="/writeCommunity" element={<WriteCommunity userId={userId} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
