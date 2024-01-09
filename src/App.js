15fca8d7d582224e2611cc5e07c0302afad663e4
oweaj <qwe1520156@naver.com>
2024-01-09 14:28:41 +0900

    [CHORE] code refactoring

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
+import React, { lazy, Suspense } from "react";
 import "./App.css";
 import { auth } from "util/fbase";
-import Main from "pages/Main/Main";
-import Map from "pages/Map/Map";
-import AboutUs from "pages/AboutUs/AboutUs";
-import Community from "pages/Community/Community";
-import SignUp from "pages/SignUp/SignUp";
-import SignIn from "pages/SignIn/SignIn";
-import MyPage from "pages/MyPage/MyPage";
 import Header from "components/Header/Header";
 import Footer from "components/Footer/Footer";
-import NotFound from "pages/NotFound/NotFound";
 import { useEffect, useState } from "react";
-import WriteCommunity from "pages/Community/WriteCommunity";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { uid, userData } from "Atom/atom";
 import { RecoilLogger } from "recoil-devtools-logger";
+
+const Main = lazy(() => import("pages/Main/Main"));
+const Map = lazy(() => import("pages/Map/Map"));
+const AboutUs = lazy(() => import("pages/AboutUs/AboutUs"));
+const Community = lazy(() => import("pages/Community/Community"));
+const WriteCommunity = lazy(() => import("pages/Community/WriteCommunity"));
+const SignIn = lazy(() => import("pages/SignIn/SignIn"));
+const SignUp = lazy(() => import("pages/SignUp/SignUp"));
+const MyPage = lazy(() => import("pages/MyPage/MyPage"));
+const NotFound = lazy(() => import("pages/NotFound/NotFound"));
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
@@ -44,18 +46,20 @@ function App() {
     <div className="App font-sans">
       <RecoilLogger values={[uid, userData]} />
       <Header isLogin={isLogin} userId={userId} userProfile={userProfile} setUserProfile={setUserProfile} />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/map" element={<Map userId={userId} />} />
-        <Route path="/signup" element={<PrivateRoute component={<SignUp />} userId={userId} />} />
-        <Route path="/signin" element={<PrivateRoute component={<SignIn />} userId={userId} />} />
-        <Route path="/mypage" element={<MyPage userId={userId} userProfile={userProfile} setUserProfile={setUserProfile} />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/community" element={<Community isLogin={isLogin} />} />
-        <Route path="/community/:id" element={<Community userId={userId} />} />
-        <Route path="/writeCommunity" element={<WriteCommunity userId={userId} />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loding...</div>}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/map" element={<Map userId={userId} />} />
+          <Route path="/signup" element={<PrivateRoute component={<SignUp />} userId={userId} />} />
+          <Route path="/signin" element={<PrivateRoute component={<SignIn />} userId={userId} />} />
+          <Route path="/mypage" element={<MyPage userId={userId} userProfile={userProfile} setUserProfile={setUserProfile} />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/community" element={<Community isLogin={isLogin} />} />
+          <Route path="/community/:id" element={<Community userId={userId} />} />
+          <Route path="/writeCommunity" element={<WriteCommunity userId={userId} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
